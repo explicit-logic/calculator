40077efb05333b4e492ee1a2b4158ffba83e15c8
Bohdan Andrianov <bogd.an.drianov@gmail.com>
2017-08-23 12:42:40 +0300

    Revert "Revert "Revert "delay for ie8"""
    This reverts commit c892487351042fd327b4c485535a717a2b8a21c2.

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 lib('>ready').tag(function(tag){
-	setTimeout(function(){
-		new Calculator(tag().div({'class':'main'}).$
-				.div({'class':'wrapper'}).$);	
-	},3000)
-	
+	new Calculator(tag().div({'class':'main'}).$
+				.div({'class':'wrapper'}).$);
 })
