4c0550509d75823fd69d43f8de37a592bf162d01
Bohdan Andrianov <Bogdan.Adrianov@bitmedia.com.ua>
2019-11-27 17:44:18 +0200

    add buttons config

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 
+import * as config from './../config/buttons.json';
+
 import {Panel, Position} from './../panel/Panel';
 import {Scene} from './../scene/Scene';
 
+console.log(config);
 export class Calculator extends React.Component {
 
   render() {
--- src/declarations/custom.d.ts
+++ src/declarations/custom.d.ts
@@ -2,3 +2,7 @@ declare module "*.svg" {
   const fileName: string;
   export default fileName;
 }
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
